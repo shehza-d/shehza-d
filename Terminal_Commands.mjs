7961e900feb9d1feea96f857d54e4450f8632297
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-09-29 16:34:45 +0500

    All commands added

---
+++ Terminal_Commands.mjs
@@ -0,0 +1,103 @@
+if you see old commands don't tell me fork it and feel free to contribute in this OpenSource Project
+$$$$$$$$$$  COMMANDS    $$$$$$$$$$$$$$$
+cd
+dir or ls (to view files in directories)
+
+%%%%%%%%%% Git Commands %%%%%%%%%%%%
+
+git add *
+
+git commit -m "apna comment"
+
+git push origin main 
+
+(
+	first time 
+	git config --global user.name "apna nam”
+
+	git config --global user.email "apna email”
+)
+
+
+%%%%%%%%%% React Commands %%%%%%%%%%%%
+
+npx create-react-app myappname   (to create react project)
+
+npm start  (to start app)
+
+
+npm run build   (after work make build folder)
+
+
+
+
+%%%%%%%%%% Node Commands %%%%%%%%%%%%
+node fileKaName
+
+%%%%%%%%%% ExpressJS Commands %%%%%%%%%%%%
+
+npm init  (to create package.json)
+
+npm install express
+
+
+
+%%%%%%%%%% FireBase Commands %%%%%%%%%%%%
+
+npm install -g firebase-tools (install CLI(also use this command to update CLI))
+
+firebase login  (first time only)
+
+firebase init (initilize project(apne project ma run karna ye))
+
+firebase deploy --only hosting
+
+firebase deploy   [(--only hosting),(--only database),(--only storage),(--only firestore),(--only functions)]
+
+
+firebase --help
+
+%%%%%%%%%% Netlify Commands %%%%%%%%%%%%
+
+npm install netlify-cli -g
+
+netlify login (first time only)
+
+!(netlify init)
+
+netlify deploy --prod
+
+
+netlify status
+
+netlify help
+
+%%%%%%%%%% Formik i Commands %%%%%%%%%%%%
+
+npm install formik --save
+
+%%%%%%%%%% yup i Commands %%%%%%%%%%%%
+
+npm i yup
+
+npm install -S yup
+
+%%%%%%%%%% Git Commands %%%%%%%%%%%%
+
+
+
+// npm init   (to initinil node project)
+// npm install express  (to initilize express project) express ky liye
+// 
+// 
+// 
+// 
+
+
+
+
+%%%%%%%%%% Git Commands %%%%%%%%%%%%
+%%%%%%%%%% Git Commands %%%%%%%%%%%%
+%%%%%%%%%% Git Commands %%%%%%%%%%%%
+%%%%%%%%%% Git Commands %%%%%%%%%%%%
+%%%%%%%%%% Git Commands %%%%%%%%%%%%
