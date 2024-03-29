0bd4c301e0b812eb0af212e18151af83c9796cb0
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-03-30 04:13:38 +0500

    Create typing.js

---
+++ typing.js
@@ -0,0 +1,11 @@
+// const isAbc = (str) => {
+//   const alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+//   return str == alphabets;
+// };
+
+// true false,const let about shehzad as it is 
+
+// while(true){
+// 	isAbc()
+// }
